379e76b30c129e3e4a8554b9e115821afd41f3ab
montrell43 <tmerriweather@persevernow.org>
2025-05-08 16:47:20 -0500

    first commit

--- myApp.js
+++ myApp.js
@@ -3,20 +3,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Root route for "/"
-app.get('/:word/echo', (req, res) => {
-  const word = req.params.word;
-  res.json({ echo: word });
-});
-
-
-// Root-level logger middleware
+// Root-level logger middleware FIRST
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-// Route: GET /json with MESSAGE_STYLE env variable
+// Root route for "/"
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express app!');
+});
+
+// Echo route
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
+
+// /json route with optional uppercase
 app.get('/json', (req, res) => {
   const message = process.env.MESSAGE_STYLE === 'uppercase'
     ? 'HELLO JSON'
@@ -24,12 +28,12 @@ app.get('/json', (req, res) => {
   res.json({ message });
 });
 
-// Route: GET /now with chained middleware
+// /now route with middleware chaining
 app.get('/now', (req, res, next) => {
-  req.time = new Date().toString(); // Add current time to req
+  req.time = new Date().toString();
   next();
 }, (req, res) => {
-  res.json({ time: req.time }); // Send time in response
+  res.json({ time: req.time });
 });
 
 module.exports = app;
